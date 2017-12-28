943808066cfeb6b07c2d50671527e02eeafd8e4a
jermspeaks <jermspeaks@gmail.com>
2017-12-28 00:33:52 -0800

    fish individual pages starter

--- src/Fish/FishIndividualPage.js
+++ src/Fish/FishIndividualPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import './FishIndividualPage.css';
 
 class FishIndividualPage extends Component {
 	static propTypes = {
@@ -9,8 +10,38 @@ class FishIndividualPage extends Component {
 	render() {
 		const { match } = this.props;
 		return (
-			<div className="fish-page">
-				<header>Name: {match.params.fishId}</header>
+			<div className="fish-individual-page">
+				<header className="fish-individual-title">
+					<h1>{match.params.fishId}</h1>
+				</header>
+				<div className="fish-individual-wrapper">
+					<section>
+						<h2>Sustainability</h2>
+						<nav>
+							<ul>
+								<li>
+									<a 
+										className="fish-individual-nav-link"
+										id="fish-individual-nav-link-overview"
+										href={`http://www.seafoodwatch.org/seafood-recommendations/groups/${match.params.fishId.toLowerCase()}/overview`} 
+										target="_blank"
+									>Overview</a>
+								</li>
+								<li>
+									<a 
+										className="fish-individual-nav-link"
+										id="fish-individual-nav-link-recommendations"
+										href={`http://www.seafoodwatch.org/seafood-recommendations/groups/${match.params.fishId.toLowerCase()}`} 
+										target="_blank"
+									>Recommendations</a>
+								</li>
+							</ul>
+						</nav>
+					</section>
+					<section>
+						<h2>Recipes</h2>
+					</section>
+				</div>
 			</div>
 		);
 	}
