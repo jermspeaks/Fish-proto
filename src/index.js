1b698e2c0dae21edf1a892141780af1a45c69d5d
jermspeaks <jermspeaks@gmail.com>
2017-12-28 01:03:39 -0800

    add service worker back

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './Root';
-// eslint-disable-next-line
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-// eslint-disable-next-line
-// registerServiceWorker();
+registerServiceWorker();
